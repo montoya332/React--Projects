29bd49ea16c3e68f9c01984ba9041ac7419f74cf
Arturo Montoya <arturo@ascendify.com>
2017-07-30 12:06:13 -0700

    Tic Tac Toe Styles

--- src/projects/ticTacToeApp/container.js
+++ src/projects/ticTacToeApp/container.js
@@ -13,27 +13,29 @@ export class MainLayoutContainer extends Component {
 		this.renderItem = this.renderItem.bind(this);
 		this.renderRow = this.renderRow.bind(this);
 	}
+	handleCellClick = (key) => {
+		console.log(key)
+	}
 	renderItem(item,key,list){
+		let cell = item;
 		const styles = {
 		    width: 60,
 		    height: 60,
 		    fontSize: '48px'
 		};
-
+		
 		if('O' === item ){
-			return <td className={`cell cell__${key+1}`} key={key}><PanoramaFishEye/></td>
+			cell = (<PanoramaFishEye />)
+		} else if('X'=== item){
+			cell = (<Clear />)
 		}
-		if('X'=== item){
-			return <td className={`cell cell__${key+1}`} key={key}><Clear/></td>
-		}
-
-		return <td className={`cell cell__${key+1}`} key={key}>{item}</td>
+		return <td onClick={()=>this.handleCellClick(key)} className={`cell cell__${key+1}`} key={key}>{cell}</td>
 	}
 	renderRow(row,key){
 		return <tr key={key}>{row.map((cell,cellKey)=>this.renderItem(cell,cellKey+key*row.length))}</tr>
 	}
 	renderBoard(){
-		const board = [['X','O','X'],['O','X','X'],['X','X','O']];
+		const board = [['X','','X'],['','','X'],['X','','O']];
 		return (
 			<table>
 				<tbody>
