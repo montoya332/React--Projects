2f99fe6ab4ba69771ea9aa3da59c0ea93f826d2a
Arturo Montoya <arturo@ascendify.com>
2017-08-14 17:20:27 -0700

    Added: Youtube example

--- src/projects/findTheBugApp/youTube/container.js
+++ src/projects/findTheBugApp/youTube/container.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Grid from 'material-ui/Grid';
+import _ from 'lodash';
+
+import YTSearch from 'youtube-api-search';
+import SearchBar from './search_bar';
+import VideoList from './video_list';
+import VideoDetail from './video_detail';
+
+const API_KEY = 'AIzaSyCw0thCQkqCOY99ejMgR72SFOsyqyiosoc';
+
 
 export class MainLayoutContainer extends Component {
 	static defaultProps = {};
 	static propTypes = {};
-	constructor(props) {
+	constructor(props) {
 		super(props);
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch('computer science');
 	}
-	
-	render() {
+
+	videoSearch = (term) => {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,// {videos} Synthactic sugar for this.setState({ videos: videos });
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render(){
+		const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300);
 		return (
 			<Grid container spacing={24}>
-				 <Grid item xs={12}>
-					test
+				<Grid item xs={12}>
+					<SearchBar onSearchTermChange={videoSearch} />
+					<VideoDetail video={this.state.selectedVideo} />
+					<VideoList
+						onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+						videos={this.state.videos} 
+					/>
 				</Grid>
 			</Grid>
-
 		);
 	}
 }
---
+++ src/projects/findTheBugApp/youTube/search_bar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
+
+  handleInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <TextField
+          id="placeholder"
+          label="Search"
+          InputProps={{ placeholder: 'Type something...' }}
+          helperText="Full width!!!"
+          fullWidth
+          margin="normal"
+          onChange={event => this.handleInputChange(event.target.value)}
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
